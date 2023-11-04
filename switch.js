8561a6505836588b99edb0d46ccd900cc7c970c7
codewithankit-coder <146661969+codewithankit-coder@users.noreply.github.com>
2023-11-04 15:51:16 +0000

    switch changes
    Basic practise of javascript using switch case

---
+++ switch.js
@@ -0,0 +1,55 @@
+
+console.log("switch case statement");
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//  let  month=5;
+ let month="march";
+switch(month){
+    // case march://ham yese bhi likh sakate hai
+    case 1:
+        console.log("january");
+        break;
+        case 2:
+            console.log("february");
+            break;
+            case 3:
+                console.log("march");
+                break;
+                case 4:
+                    console.log("april");
+                    break;
+   case 5:
+       console.log("may");
+    break;
+       case 6:
+                            consle.log("june");
+           break;
+         case 7:
+        console.log("july");
+                                break;
+              case 8:
+                  console.log("august");
+                   break;
+         case 9:
+                console.log("September");
+                break;
+                case 10:
+                    console.log("october");
+                    break;
+                      case 11:
+                        console.log("November");
+                        break;
+                        case 12 :
+                            console.log("December");
+                            default:
+                                console.log("please enter valid number");
+                                         
+}
